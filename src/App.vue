cabfd06693b1db8c8e46b68b2749db9f97f68703
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-26 16:05:39 +0200

    create API GET
    create API sia per archetipi che per le carte

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,28 @@
 <script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      archetypes: null,
+      card: null
+    }
+  },
+  created() {
+    axios
+      .get('https://db.ygoprodeck.com/api/v7/archetypes.php')
+      .then(response => {
+        this.archetypes = response.data;
+      }),
+      axios
+        .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+        .then(response => {
+          this.cards = response.data.data;
+          console.log(this.cards[0])
+        })
+  }
+}
+
 </script>
 
 <template>
@@ -10,10 +34,24 @@
     </div>
   </header>
   <main class="py-4">
-    <div class="cards-container container">
-      ciao
+
+
+    <div class="container">
+
+      <div class="col-3 pb-3">
+        <select class="form-select" aria-label="select example">
+          <option selected>Open this select menu</option>
+          <option v-for="archetype in archetypes" :value="archetype.archetype_name">
+            {{ archetype.archetype_name }}
+          </option>
+        </select>
+      </div>
+
+      <div class="cards-container">
+        ciao
+      </div>
     </div>
   </main>
 </template>
 
-<style></style>
+<style lang="scss"></style>
