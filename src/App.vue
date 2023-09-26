380b2856d603358b2a4ef34dabb70b9b42427bdf
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-26 14:42:36 +0200

    initial commit
    configurata app vite con sass e bootstrap

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+  <div class="container">
+    <h1>
+      hello vue
+    </h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import './assets/scss/app.scss'
+
+createApp(App).mount('#app')
