7921046a1556737062bf3df1fac36bbc73e6a8e3
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-26 15:01:41 +0200

    struttura base
    impostata struttura pagina

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 </script>
 
 <template>
-  <div class="container">
-    <h1>
-      hello vue
-    </h1>
-  </div>
+  <header>
+    <div class="p-4 pb-1">
+      <h1>
+        Yu-Gi-Oh Api
+      </h1>
+    </div>
+  </header>
+  <main class="py-4">
+    <div class="cards-container container">
+      ciao
+    </div>
+  </main>
 </template>
 
 <style></style>
