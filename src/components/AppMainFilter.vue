8cf6bad66321c62e4105e337be773f1d35f6a0e9
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-27 16:55:37 +0200

    modifiche style

--- src/components/AppMainFilter.vue
+++ src/components/AppMainFilter.vue
@@ -13,32 +13,28 @@ export default {
             <div class="input-group-text text-bg-dark">
                 Filter by Archetype
             </div>
-            <div>
-                <select class="form-select" aria-label="select example" id="archetypeFilter" @change="$emit('filter')">
-                    <option selected value="null">
-                        No filter
-                    </option>
-                    <option option v-for=" archetype in archetypes" :value="archetype.archetype_name">
-                        {{ archetype.archetype_name }}
-                    </option>
-                </select>
-            </div>
+            <select class="form-select" aria-label="select example" id="archetypeFilter" @change="$emit('filter')">
+                <option selected value="null">
+                    No filter
+                </option>
+                <option option v-for=" archetype in archetypes" :value="archetype.archetype_name">
+                    {{ archetype.archetype_name }}
+                </option>
+            </select>
         </div>
 
         <div class="input-group ms-2">
             <div class="input-group-text text-bg-dark">
                 Number of cards to show
             </div>
-            <div>
-                <select class="form-select" aria-label="select example" id="numbFilter" @change="$emit('filter')">
-                    <option selected value=5>5</option>
-                    <option value=10>10</option>
-                    <option value=20>20</option>
-                    <option value=50>50</option>
-                    <option value=100>100</option>
-                    <option value=200>200</option>
-                </select>
-            </div>
+            <select class="form-select" aria-label="select example" id="numbFilter" @change="$emit('filter')">
+                <option selected value=5>5</option>
+                <option value=10>10</option>
+                <option value=20>20</option>
+                <option value=50>50</option>
+                <option value=100>100</option>
+                <option value=200>200</option>
+            </select>
         </div>
     </div>
 </template>
